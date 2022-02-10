7e2cd7e58be6da2b7b37a72880e4a775b8ea17aa
amit <amit.mankotia@nssplindia.com>
2022-02-10 23:37:05 +0530

    initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function Navbar() {
+  return (
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <button>Dashboard</button>
+  </div>
+</nav>
+  )
+}
