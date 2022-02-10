a0e0425ac030e75a1336196afd3de661898e8164
amit <amit.mankotia@nssplindia.com>
2022-02-10 23:40:18 +0530

    normal commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Login from './Components/Login';
 import CSI from './Components/CSI';
 function App() {
-return (
-<Router>
-<Routes>
-<Route path="/" element={<Login />} />
-<Route exact path="/CSI" element={<CSI />} />
-</Routes>
-</Router>
-)
+
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route exact path="/CSI" element={<CSI />} />
+            </Routes>
+        </Router>
+    )
 }
 export default App;
