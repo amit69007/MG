a2f22b963bd079a2e2f179fee61530a61a03fcea
Amit Mankotia <74097257+amit69007@users.noreply.github.com>
2022-02-23 10:18:25 +0530

    call

--- src/Components/CSI.js
+++ src/Components/CSI.js
@@ -15,150 +15,106 @@ setFirstErr(true);
 setFirstErr(false);
 }
 };
-  const LastHandler = (Event) => {
-    let item = Event.target.value;
-    if (item.length < 6) {
-      setLastErr(true);
-    } else {
-      setLastErr(false);
-    }
-  };
-  const emailValidation = () => {
-    const regEx = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
-    if (regEx.test(email)) {
-      setMessage("Email is Valid");
-    } else if (!regEx.test(email) && email !== "") {
-      setMessage("Email is Not Valid");
-    } else {
-      setMessage("");
-    }
-  };
-  const handleOnChange = (event) => {
-    setEmail(event.target.value);
-  };
-  const [Modal, setModal] = useState(0);
-  const toggleModal = () => {
-    setModal(!Modal);
-  };
-  return (
-    <>
-      <div className="block">
-        <img src="./MG.svg" alt="Logo" className="Logo" />     
-            <Link type="button" className="btn mx-5" id="now" to="/Navbar">
-              <b>CSI</b>
-            </Link>  
-            <button
-              type="button"
-              id="myBtn"
-              onClick={toggleModal}
-              className="btn">
-              <b>SSI</b>
-            </button>     
-      </div>
-      <button type="button" className="icon">
-        <span className="material-icons" id="out">
-          person
-        </span>
-      </button>
-      
-      {Modal ? (
-        <div className="modal">
-          <div className="container" id="for">
-            <h5>
-              <b>Register New User</b>
-            </h5>
-            <div className=" Drake form-floating">
-              <input
-                type="email"
-                className="Ikaris form-control"
-                id="floatingInput"
-                placeholder="First Name"
-                onChange={firstHandler}
-              />
-              {firstErr ? <span>First Name Required</span> : ""}
-              <label className="Form" htmlFor="floatingInput">
-                First Name
-              </label>
-            </div>
-            <div className="Eminem form-floating">
-<input type="email"className="Ikaris form-control" id="floatingInput" placeholder="Email" value={email}  onChange={handleOnChange} onClick={emailValidation}/>          
-    {/* <spam id="sos">{message}</spam> */}
-             <label className="Form" htmlFor="floatingInput">
-                Email
-              </label>
-            </div> 
-            <span id="sos">{message}</span>
-            <div className="Taylor form-floating">
-              <input
-                type="email"
-                className="Ikaris form-control"
-                id="floatingInput"
-                placeholder="Last Name"
-                onChange={LastHandler}
-              />
-              {lastErr ? <span>Last Name Required</span> : ""}
-              <label className="Form" htmlFor="floatingInput">
-                Last Name
-              </label>
-            </div>
-
-            <div className="Halsey">
-              <label htmlFor="exampleInputEmail1" className="form-label">
-                Access To Panel (Multiple)
-              </label>
-              <input
-                type="email"
-                className=" form-control"
-                id="exampleInputEmail1"
-              />
-            </div>
-            <div className="Rana">
-              <label htmlFor="exampleInputEmail1" className="form-label">
-                Regions (Multiple)
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="exampleInputEmail1"
-              />
-            </div>
-            <div className="Draco">
-              <label htmlFor="exampleInputEmail1" className="form-label">
-                States (Multiple)
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="exampleInputEmail1"
-              />
-            </div>
-            <div className="Leo">
-              <label htmlFor="exampleInputEmail1" className="form-label">
-                Area (Multiple)
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="exampleInputEmail1"
-              />
-            </div>
-            <div className="Tom">
-              <label htmlFor="exampleInputEmail1" className="form-label">
-                CSI Outlets (Multiple)
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="exampleInputEmail1"
-              />
-            </div>
-            <div className="Hardy">
-              <label htmlFor="exampleInputEmail1" className="form-label">
-                SSI Outlets (Multiple)
-              </label>
-<input type="email" className="form-control" id="exampleInputEmail1"/>
+const LastHandler = (Event) => {
+let item = Event.target.value;
+if (item.length < 6) {
+setLastErr(true);
+} else {
+setLastErr(false);
+}
+};
+const emailValidation = () => {
+const regEx = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
+if (regEx.test(email)) {
+setMessage("Email is Valid");
+} else if (!regEx.test(email) && email !== "") {
+setMessage("Email is Not Valid");
+} else {
+setMessage("");
+}
+};
+const handleOnChange = (event) => {
+setEmail(event.target.value);
+};
+const [Modal, setModal] = useState(0);
+const toggleModal = () => {
+setModal(!Modal);
+};
+return (
+<>
+<div className="block" >
+<div className="ground">
+<img src="./MG.svg" alt="Logo" className="Logo"/>
+<div className="stan">
+<Link type="button" className="btn mx-5" id="now" to="/Navbar">
+<b>CSI</b>
+</Link>
+<button
+type="button"
+id="myBtn"
+onClick={toggleModal}
+className="btn">
+<b id="wall">SSI</b>
+</button>
 </div>
-<div className="Lord">
+</div>     
+<button type="button" className="icon">
+<span className="material-icons" id="out">
+person
+</span>
+</button>
+</div>
+{Modal ? (
+<div className="modal ">
+<div className="container" id="for">
+<div id="hi">
+<b>Register New User</b>
+</div>
+<div id="ray">
+<button type="button" className="icon-button">
+<span className="material-icons" onClick={toggleModal}>
+highlight_off
+</span>
+</button>
+</div>
+<div className="bow">
+<div className=" Drake form-floating mx-4">
+<input
+type="email"
+className="Ikaris form-control"
+id="floatingInput"
+placeholder="First Name"
+onChange={firstHandler}
+/>
+{firstErr ? <span>First Name Required</span> : ""}
+<label className="Form" htmlFor="floatingInput">
+First Name
+</label>
+</div>   
+<div className="Taylor form-floating mx-4">
+<input
+type="email"
+className="Ikaris form-control"
+id="floatingInput"
+placeholder="Last Name"
+onChange={LastHandler}
+/>
+{lastErr ? <span>Last Name Required</span> : ""}
+<label className="Form" htmlFor="floatingInput">
+Last Name
+</label>
+</div>
+</div>
+<div className="delta">
+<div className="Eminem form-floating mx-4">
+<input type="email"className="Ikaris form-control" id="floatingInput" placeholder="Email" value={email}  onChange={handleOnChange} onClick={emailValidation}/>          
+<spam id="sos">{message}</spam>
+<label className="Form" htmlFor="floatingInput">
+Email
+</label>
+</div> 
+<span id="sos">{message}</span>
+<div className="Lord mx-4">
 <label htmlFor="floatingInput">Designation</label>
 <select className="form-select">
 <option value="1">One</option>
@@ -167,17 +123,77 @@ setFirstErr(false);
 <option value="4">Four</option>
 </select>
 </div>
-<button type="submit" className="btn" id="zeus">
-<b>Register</b>
-</button>
-<button type="button" className="icon-button">
-<span className="material-icons" onClick={toggleModal}>
-highlight_off
-</span>
-</button>
+</div>
+<div className="thor">
+<div className="Halsey mx-4">
+<label htmlFor="exampleInputEmail1" className="form-label">
+Access To Panel (Multiple)
+</label>
+<input
+type="email"
+className=" form-control"
+id="exampleInputEmail1"
+/>
+</div>
+<div className="Rana mx-4">
+<label htmlFor="exampleInputEmail1" className="form-label">
+Regions (Multiple)
+</label>
 <button type="select" id="btn1" className="btn">
 <b>Select All</b>
 </button>
+<input
+type="email"
+className="form-control"
+id="exampleInputEmail1"
+/>
+</div>
+</div>
+<div className="lid">
+<div className="Draco mx-4">
+<label htmlFor="exampleInputEmail1" className="form-label">
+States (Multiple)
+</label>
+<input
+type="email"
+className="form-control"
+id="exampleInputEmail1"
+/>
+</div>
+<div className="Leo mx-4">
+<label htmlFor="exampleInputEmail1" className="form-label">
+Area (Multiple)
+</label>
+<input
+type="email"
+className="form-control"
+id="exampleInputEmail1"
+/>
+</div>
+</div>
+<div className="war">
+<div className="Tom mx-4">
+<label htmlFor="exampleInputEmail1" className="form-label">
+CSI Outlets (Multiple)
+</label>
+<input
+type="email"
+className="form-control"
+id="exampleInputEmail1"
+/>
+</div>
+<div className="Hardy mx-4">
+<label htmlFor="exampleInputEmail1" className="form-label">
+SSI Outlets (Multiple)
+</label>
+<input type="email" className="form-control" id="exampleInputEmail1"/>
+</div>
+</div>
+<div id="dua">
+<button type="submit" className="btn" id="zeus">
+<b>Register</b>
+</button>
+</div>
 </div>
 </div>
 ):(
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,71 +2,66 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
 export default function Login() {
-  const [PassErr, setPassErr] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  function passwordHandler(event) {
-    const item = event.target.value;
-    if (item.length < 6) {
-      setPassErr(true);
-    } else {
-      setPassErr(false);
-    }
-  }
-  const emailValidation = () => {
-    const regEx = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
-    if (regEx.test(email)) {
-      setMessage("Email is Valid");
-    } else if (!regEx.test(email) && email !== "") {
-      setMessage("Email is Not Valid");
-    } else {
-      setMessage("");
-    }
-  };
-  const handleOnChange = (event) => {
-    setEmail(event.target.value);
-  };
-  return (
-    <div className="login_container">
-      <div className="login-form">
-        <img src="./MG logo.svg" alt="Logo" className="Logo1 mb-3" />
-        <form style={{ width: "80%" }}>
-          <div className="form-floating mb-5">
-            <input
-              type="email"
-              className="form-control"
-              id="floatingInput"
-              placeholder="Email"
-              value={email}
-              onChange={handleOnChange}
-              onClick={emailValidation}
-              style={{ width: "100%" }}
-            />
-            <p>{message}</p>
-            <label htmlFor="floatingInput" className="Form1">
-              Email
-            </label>
-          </div>
-          <div className="form-floating mb-3">
-            <input
-              type="password"
-              className="form-control"
-              id="floatingPassword"
-              onChange={passwordHandler}
-              placeholder="Password"
-              style={{ width: "100%" }}
-            />
-            {PassErr ? <span>Password is Not Valid</span> : ""}
-            <label htmlFor="floatingPassword" className="Form2">
-              Password
-            </label>
-          </div>
-          <Link type="click" className="btn btn-dark" id="link" to="/CSI">
-            <b>Login</b>
-          </Link>
-          <div style={{ float: "left", padding: "5px" }}>Forgot Password?</div>
-        </form>
-      </div>
-    </div>
-  );
+const [PassErr, setPassErr] = useState("");
+const [email, setEmail] = useState("");
+const [message, setMessage] = useState("");
+function passwordHandler(event) {
+const item = event.target.value;
+if (item.length < 6) {
+setPassErr(true);
+} else {
+setPassErr(false);
+}
+}
+const emailValidation = () => {
+const regEx = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
+if (regEx.test(email)) {
+setMessage("Email is Valid");
+} else if (!regEx.test(email) && email !== "") {
+setMessage("Email is Not Valid");
+} else {
+setMessage("");
+}
+};
+const handleOnChange = (event) => {
+setEmail(event.target.value);
+};
+return (
+<div className="login_container">
+<div className="login-form">
+<img src="./MG logo.svg" alt="Logo" className="Logo1 mb-3" />
+<form style={{ width: "80%" }}>
+<div className="form-floating mb-5">
+<input type="email" className="form-control" id="floatingInput" placeholder="Email" value={email}
+onChange={handleOnChange}
+onClick={emailValidation}
+style={{ width: "100%" }}
+/>
+<p>{message}</p>
+<label htmlFor="floatingInput" className="Form1">
+Email
+</label>
+</div>
+<div className="form-floating mb-3">
+<input
+type="password"
+className="form-control"
+id="floatingPassword"
+onChange={passwordHandler}
+placeholder="Password"
+style={{ width: "100%" }}
+/>
+{PassErr ? <span>Password is Not Valid</span> : ""}
+<label htmlFor="floatingPassword" className="Form2">
+Password
+</label>
+</div>
+<Link type="click" className="btn btn-dark" id="link" to="/CSI">
+<b>Login</b>
+</Link>
+<div style={{ float: "left", padding: "5px" }}>Forgot Password?</div>
+</form>
+</div>
+</div>
+);
 }
